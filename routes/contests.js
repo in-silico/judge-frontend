632cba3e1f20b3fd608647deb32abdb21e25447f
Carlos Gonzalez <caal.0522@gmail.com>
2016-02-17 12:57:59 -0500

    fixed error

--- routes/contests.js
+++ routes/contests.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router  = express.Router();
 
-module.exports = function(app) {
-  router.get('/contests', function(req, res) {
+module.exports = function(app, mountPoint) {
+  router.get('/', function(req, res) {
     res.render('index', {title : "UTPJudge"});
   });
 
-  router.get('/contests/new', function(req, res) {
+  router.get('/new', function(req, res) {
     res.render('index', {title : "UTPJudge"});
   });
 
