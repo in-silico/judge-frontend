c00c6c9e78e45b076e2c1e3d86e8afde587e4c7e
Carlos Gonzalez <carlos.23325@gmail.com>
2016-02-22 00:56:39 -0500

    Using routes for problems

--- routes/problems.js
+++ routes/problems.js
@@ -1,5 +1,19 @@
-module.exports = function(app) {
-  app.get('/problems', function(req, res) {
+var express = require('express');
+var router  = express.Router();
+
+module.exports = function(app, mountPoint) {
+  router.get('/', function(req, res) {
     res.render('index', {title : "UTPJudge"});
   });
+
+  router.get('/new', function(req, res) {
+    res.render('index', {title : "UTPJudge"});
+  });
+
+  router.get('/:id', function(req, res) {
+    res.render('index', {title : "UTPJudge"});
+  });
+
+
+  app.use(mountPoint, router);
 }
